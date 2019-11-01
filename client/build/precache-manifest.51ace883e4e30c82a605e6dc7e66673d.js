8fcf0d8dcb81127eeffa1570a8f9387419cefdba
JakobLyon <jlyon32893@gmail.com>
2019-11-01 19:36:56 -0400

    moved client files into client folder, created nginx and server folders with associated docker setup

---
+++ client/build/precache-manifest.51ace883e4e30c82a605e6dc7e66673d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cbaec0f0127016d04b82268edcae8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adafee859af42f2b7bd2",
+    "url": "/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "d73f31b49f52713337ce",
+    "url": "/static/css/main.3986cfeb.chunk.css"
+  },
+  {
+    "revision": "adafee859af42f2b7bd2",
+    "url": "/static/js/2.6f99db4c.chunk.js"
+  },
+  {
+    "revision": "d73f31b49f52713337ce",
+    "url": "/static/js/main.cd33af90.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
