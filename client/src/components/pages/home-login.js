3894817052263ad5b1d1a91097c47d8113145205
JakobLyon <jlyon32893@gmail.com>
2019-11-12 17:20:52 -0500

    added node-sass, refactored out Radium in home-login for sass

--- client/src/components/pages/home-login.js
+++ client/src/components/pages/home-login.js
@@ -2,27 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Login } from "../login";
 import { Redirect } from "react-router-dom";
-import Radium from "radium";
+import "./home-login.scss";
 
 import { connect } from "react-redux";
 import { isLoggedIn } from "../../redux/selectors/temporal-boon-selectors";
 
 import { logIn } from "../../redux/actions/log-in";
 import { createUser } from "../../redux/actions/create-user";
-
-const styles = {
-  outer: {
-    position: "relative",
-    width: "100%",
-    height: "50vh"
-  },
-  inner: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)"
-  }
-};
 
 const mapStateToProps = state => ({
   isLoggedIn: isLoggedIn(state)
@@ -32,8 +18,8 @@ const HomeLoginComponent = ({ isLoggedIn, logIn, createUser }) =>
   isLoggedIn ? (
     <Redirect to="/cooldowns" />
   ) : (
-    <div data-enzyme-id="home-login-component" style={[styles.outer]}>
-      <div style={[styles.inner]}>
+    <div data-enzyme-id="home-login-component" className="outer">
+      <div className="inner">
         <Login handleLogIn={logIn} handleCreateUser={createUser} />
       </div>
     </div>
@@ -56,4 +42,4 @@ export default HomeLoginComponent;
 export const HomeLogin = connect(
   mapStateToProps,
   { logIn, createUser }
-)(Radium(HomeLoginComponent));
+)(HomeLoginComponent);
