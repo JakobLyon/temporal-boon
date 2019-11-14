e628b0d82c3d824d28bc55a07dd378218cd0bc5e
JakobLyon <jlyon32893@gmail.com>
2019-11-14 14:19:46 -0500

    slight refactor to test

--- client/src/components/__tests__/home-login.test.js
+++ client/src/components/__tests__/home-login.test.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { shallow } from "enzyme";
 import HomeLoginComponent from "../pages/home-login";
+import { testStore } from "../../../utils";
 
+const setUp = (initialState = {}) => {
+  const store = testStore(initialState);
+  return shallow(<HomeLoginComponent store={store} />);
+};
 
 describe("<HomeLoginComponent/>", () => {
-  // isLoggedIn => false
-  // --- login renders
-  it("Given isLoggedIn as false, render Login", () => {
-    const wrapper = shallow(<HomeLoginComponent isLoggedIn={false}/>);
-    expect(wrapper.find('[data-enzyme-id="home-login-component"]').length).toBe(1);
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setUp();
   });
-  
+
+  it("Logged in default false, render Login component", () => {
+    expect(wrapper.find('[data-enzyme-id="home-login-component"]').length).toBe(
+      1
+    );
+  });
+
   // isLoggedIn => true
   // --- login _does not_ render
   it("Given isLoggedIn as true, do NOT render Login", () => {
-    const wrapper = shallow(<HomeLoginComponent isLoggedIn={true}/>);
-    expect(wrapper.find('[data-enzyme-id="home-login-component"]').length).toBe(0);
+    const wrapper = shallow(<HomeLoginComponent isLoggedIn={true} />);
+    expect(wrapper.find('[data-enzyme-id="home-login-component"]').length).toBe(
+      0
+    );
   });
 });
